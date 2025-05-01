407b30762e1e882e76e7a87a4476fb3efda85730
MiqWic <bredanbyrum@gmail.com>
2025-05-01 16:19:06 -0400

    Create User.java

---
+++ User.java
@@ -0,0 +1,12 @@
+    // user class to hold balance info
+public class User {
+    public String username;
+    public double checkingBalance;
+    public double savingsBalance;
+
+    public User(String username, double checkingBalance, double savingsBalance) {
+        this.username = username;
+        this.checkingBalance = checkingBalance;
+        this.savingsBalance = savingsBalance;
+    }
+}
