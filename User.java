2db1fe674b288b3decb21f3e50bf8a200b301d06
MiqWic <bredanbyrum@gmail.com>
2025-05-06 15:25:11 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,33 +1,30 @@
 public class User {
     private String username;
+    private String password;
     private double checkingBalance;
     private double savingsBalance;
 
-    public User(String username, double checkingBalance, double savingsBalance) {
+    public User(String username, double checking, double savings) {
         this.username = username;
-        this.checkingBalance = checkingBalance;
-        this.savingsBalance = savingsBalance;
+        this.password = ""; // default blank
+        this.checkingBalance = checking;
+        this.savingsBalance = savings;
     }
 
-    // Getters
-    public String getUsername() {
-        return username;
+    public User(String username, String password, double checking, double savings) {
+        this.username = username;
+        this.password = password;
+        this.checkingBalance = checking;
+        this.savingsBalance = savings;
     }
 
-    public double getCheckingBalance() {
-        return checkingBalance;
-    }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
 
-    public double getSavingsBalance() {
-        return savingsBalance;
-    }
+    public double getCheckingBalance() { return checkingBalance; }
+    public void setCheckingBalance(double checkingBalance) { this.checkingBalance = checkingBalance; }
 
-    // Setters
-    public void setCheckingBalance(double checkingBalance) {
-        this.checkingBalance = checkingBalance;
-    }
-
-    public void setSavingsBalance(double savingsBalance) {
-        this.savingsBalance = savingsBalance;
-    }
+    public double getSavingsBalance() { return savingsBalance; }
+    public void setSavingsBalance(double savingsBalance) { this.savingsBalance = savingsBalance; }
 }
