4221ca02137d5c92f2322246706ad1bbab33254b
MiqWic <bredanbyrum@gmail.com>
2025-05-06 15:54:55 -0400

    Update User.java

--- User.java
+++ User.java
@@ -18,16 +18,15 @@ public class User {
         this.savingsBalance = savingsBalance;
     }
 
-    // Getters
     public String getUsername() { return username; }
     public String getPassword() { return password; }
     public String getField3() { return field3; }
     public String getField4() { return field4; }
     public String getField5() { return field5; }
-    public double getCheckingBalance() { return checkingBalance; }
-    public double getSavingsBalance() { return savingsBalance; }
 
-    // Setters
+    public double getCheckingBalance() { return checkingBalance; }
     public void setCheckingBalance(double balance) { this.checkingBalance = balance; }
+
+    public double getSavingsBalance() { return savingsBalance; }
     public void setSavingsBalance(double balance) { this.savingsBalance = balance; }
 }
