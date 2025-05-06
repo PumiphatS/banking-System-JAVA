a6415823bfcd7b73e98ff6fb8862ffa183f369ce
MiqWic <bredanbyrum@gmail.com>
2025-05-06 15:49:12 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,30 +1,33 @@
 public class User {
     private String username;
     private String password;
+    private String field3;
+    private String field4;
+    private String field5;
     private double checkingBalance;
     private double savingsBalance;
 
-    public User(String username, double checking, double savings) {
-        this.username = username;
-        this.password = ""; // default blank
-        this.checkingBalance = checking;
-        this.savingsBalance = savings;
-    }
-
-    public User(String username, String password, double checking, double savings) {
+    public User(String username, String password, String field3, String field4, String field5,
+                double checkingBalance, double savingsBalance) {
         this.username = username;
         this.password = password;
-        this.checkingBalance = checking;
-        this.savingsBalance = savings;
+        this.field3 = field3;
+        this.field4 = field4;
+        this.field5 = field5;
+        this.checkingBalance = checkingBalance;
+        this.savingsBalance = savingsBalance;
     }
 
+    // Getters
     public String getUsername() { return username; }
     public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
-
+    public String getField3() { return field3; }
+    public String getField4() { return field4; }
+    public String getField5() { return field5; }
     public double getCheckingBalance() { return checkingBalance; }
-    public void setCheckingBalance(double checkingBalance) { this.checkingBalance = checkingBalance; }
-
     public double getSavingsBalance() { return savingsBalance; }
-    public void setSavingsBalance(double savingsBalance) { this.savingsBalance = savingsBalance; }
+
+    // Setters
+    public void setCheckingBalance(double balance) { this.checkingBalance = balance; }
+    public void setSavingsBalance(double balance) { this.savingsBalance = balance; }
 }
