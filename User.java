7fe59d544e1a6722d5dae7a20c2ca55e26b9f3cd
MiqWic <bredanbyrum@gmail.com>
2025-05-06 14:12:08 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,12 +1,17 @@
-    // user class to hold balance info
+// user class to hold balance info and update it accordingly
 public class User {
     public String username;
     public double checkingBalance;
     public double savingsBalance;
+    public double totalDeposited;
+    public double totalTransferred;
 
-    public User(String username, double checkingBalance, double savingsBalance) {
+    public User(String username, double checkingBalance, double savingsBalance,
+                double totalDeposited, double totalTransferred) {
         this.username = username;
         this.checkingBalance = checkingBalance;
         this.savingsBalance = savingsBalance;
+        this.totalDeposited = totalDeposited;
+        this.totalTransferred = totalTransferred;
     }
 }
