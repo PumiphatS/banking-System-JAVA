68e21370cf62ad15c6603ff7862f399a91b711bc
MiqWic <bredanbyrum@gmail.com>
2025-05-06 16:05:48 -0400

    Update User.java

--- User.java
+++ User.java
@@ -1,28 +1,28 @@
 public class User {
     private String username;
     private String password;
-    private String field3;
-    private String field4;
-    private String field5;
+    private String firstName;
+    private String lastName;
+    private String accountNumber;
     private double checkingBalance;
     private double savingsBalance;
 
-    public User(String username, String password, String field3, String field4, String field5,
+    public User(String username, String password, String firstName, String lastName, String accountNumber,
                 double checkingBalance, double savingsBalance) {
         this.username = username;
         this.password = password;
-        this.field3 = field3;
-        this.field4 = field4;
-        this.field5 = field5;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.accountNumber = accountNumber;
         this.checkingBalance = checkingBalance;
         this.savingsBalance = savingsBalance;
     }
 
     public String getUsername() { return username; }
     public String getPassword() { return password; }
-    public String getField3() { return field3; }
-    public String getField4() { return field4; }
-    public String getField5() { return field5; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getAccountNumber() { return accountNumber; }
 
     public double getCheckingBalance() { return checkingBalance; }
     public void setCheckingBalance(double balance) { this.checkingBalance = balance; }
